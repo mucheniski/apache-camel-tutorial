2e537c446dec773681cbfdea5d53b0646363426c
mucheniski <mucheniski@gmail.com>
2021-04-21 19:59:17 -0300

    Working with files

---
+++ camel-microservice-a/src/main/java/com/example/camelmicroservicea/routes/b/MyFileRouter.java
@@ -0,0 +1,24 @@
+package com.example.camelmicroservicea.routes.b;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+// https://github.com/in28minutes/camel
+
+@Component
+public class MyFileRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        /*
+        * when the process run, everting in puting in the input folger will be moved to output folder
+        * */
+
+        from("file:files/input")
+                .log("${body}")
+                .to("file:files/output");
+
+    }
+
+}
